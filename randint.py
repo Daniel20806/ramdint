565b780018b456849aa9d9171a04037d4e0529d6
Daniel20806 <angus20806@gmail.com>
2020-06-10 20:34:12 +0800

    test commit

---
+++ randint.py
@@ -0,0 +1,16 @@
+import random
+
+r = random.randint(1, 10)
+while True:
+	num = input('請猜一個數字(1~10): ')
+	num = int(num)
+	if  r - num < 0 :
+		ans = '小'
+	else:
+		ans = '大'
+	if num == r:
+		print('猜對了喔! ')
+		break
+	elif num != r:
+		print('再猜一次喔!! 數字比',num, '還',ans, '!')
+
